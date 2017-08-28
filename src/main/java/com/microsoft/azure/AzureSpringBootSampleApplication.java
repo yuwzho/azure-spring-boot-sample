6be4c3c47ef3aa0a09866329699ade76510c0e8e
Yuwei Zhou <yuwzho@microsoft.com>
2017-08-28 16:50:32 +0800

    init

---
+++ src/main/java/com/microsoft/azure/AzureSpringBootSampleApplication.java
@@ -0,0 +1,12 @@
+package com.microsoft.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringBootSampleApplication.class, args);
+	}
+}
